64b26365900053efd7928640cd353f2390a8a66c
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-03-24 01:49:52 +0530

    new roadmap structured

--- src/containers/home/components/Roadmap.tsx
+++ src/containers/home/components/Roadmap.tsx
@@ -19,65 +19,70 @@ const Roadmap = () => {
 				</Text>
 			</Box>
 			<Box center column>
-				<RoadmapComp
-					status="done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-					odd
-				/>
-				<RoadmapComp
-					status="progress"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-				/>
-				<RoadmapComp
-					status="not done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-					odd
-				/>
-				<RoadmapComp
-					status="not done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-				/>
-				<RoadmapComp
-					status="not done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-					odd
-				/>
-				<RoadmapComp
-					status="not done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-				/>
-				<RoadmapComp
-					status="not done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-					odd
-				/>
-				<RoadmapComp
-					status="not done"
-					text="Launch Simplr Collection on Polygon and Ethereum"
-					headText="Q1, 2022"
-					arrowTop="35%"
-				/>
 				<Box
 					width="5px"
-					height="80px"
-					borderBottomLeftRadius="36px"
-					borderBottomRightRadius="36px"
-					backgroundColor="#E0EAFF"
+					height="1150px"
+					borderRadius="36px"
+					backgroundImage="linear-gradient(180deg, #4743C5 10.31%, rgba(71, 67, 197, 0) 19.3%),
+					linear-gradient(0deg, #A0A8D7, #A0A8D7);
+					"
 				></Box>
+				<Box position="absolute" column>
+					<RoadmapComp
+						status="done"
+						text="Launch Simplr Collection on Polygon and Ethereum"
+						headText="2022, Q1"
+						positionMob="-27%"
+						positionDesk="-27%"
+						arrowTop="35%"
+					/>
+					<RoadmapComp status="progress" text="Make Draft feature live" headText="Q2" arrowTop="35%" odd />
+					<RoadmapComp
+						status="not done"
+						text="Exclusive community for SEAT holders goes live"
+						headText="Q2"
+						arrowTop="10%"
+						positionMob="-15%"
+					/>
+					<RoadmapComp status="not done" text="Polish Simplr Collection App" headText="Q3" arrowTop="35%" odd />
+					<RoadmapComp
+						status="not done"
+						text="Roll out new Collection type."
+						headText="Q3"
+						arrowTop="35%"
+						positionMob="-15%"
+					/>
+					<RoadmapComp
+						status="not done"
+						text="Partner with multiple NFT communities"
+						headText="Q3"
+						arrowTop="35%"
+						odd
+					/>
+					<RoadmapComp
+						status="not done"
+						text="Launch NFT marketplace for Primary Sales"
+						headText="Q3"
+						arrowTop="35%"
+						positionMob="-15%"
+					/>
+					<RoadmapComp
+						status="not done"
+						text="Adding Analytics to Creator Dashboard "
+						headText="Q4"
+						arrowTop="35%"
+						odd
+					/>
+					<RoadmapComp
+						status="not done"
+						text="Simplr Collection goes Multichain"
+						headText="2023, Q2"
+						arrowTop="35%"
+						positionMob="-27%"
+						positionDesk="-27%"
+					/>
+					<RoadmapComp status="not done" text="Simplr Bridge Beta goes live" headText="Q4" arrowTop="35%" odd />
+				</Box>
 			</Box>
 		</Box>
 	);
