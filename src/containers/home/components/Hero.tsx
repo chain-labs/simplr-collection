0b53f845c2c04762304478b2bebc3fbdff23f146
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-05-03 14:31:55 +0530

    animation using react-spring

--- src/containers/home/components/Hero.tsx
+++ src/containers/home/components/Hero.tsx
@@ -6,13 +6,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import ResponsiveText from 'src/components/ResponsiveText';
 import { WAITLIST_FORM_URL } from './constants';
 import Image from 'next/image';
+import { useSpring, animated as a } from 'react-spring';
 
 import heroImage from 'public/static/images/desktop/hero_image.png';
 import infoImage from 'public/static/images/desktop/about.png';
+import React from 'react';
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+	const styles = useSpring({
+		loop: { reverse: true },
+		from: { y: -100 },
+		to: { y: 0 },
+	});
+
+	const [greetingStatus, displayGreeting] = React.useState(true);
+	const contentProps = useSpring({
+		opacity: greetingStatus ? 1 : 0,
+		marginTop: greetingStatus ? 0 : -500,
+	});
 	return (
 		<Box display="flex" justifyContent="center" width="100vw">
 			<Box
@@ -20,46 +33,65 @@ const Hero = () => {
 				mx="auto"
 				pt={{ mobS: 'wxl', tabS: '17rem', deskM: '19rem' }}
 			>
-				<Box column center>
-					<ResponsiveText
-						mob="h2"
-						tab="h1"
-						desk="h1"
-						text="Simplified platform for creators to launch NFT collections quickly and economically."
-						color="simply-blue"
-						textAlign="center"
-						maxWidth={{ mobS: '27rem', tabS: '50rem', deskM: '59rem' }}
-						id="hero-headline"
-						lineHeight="120%"
-						mb="2%"
-					/>
-					<Text as="b2" maxWidth={{ mobS: '25rem', tabS: '53rem' }} textAlign="center" id="hero-subtext">
-						A simple, easy to use, no-code platform to create NFT smart contracts and launch your NFT projects without
-						any hassle.
-					</Text>
-					<Box
-						column
-						flexDirection={{ mobS: 'column-reverse', tabS: 'column' }}
-						alignItems="center"
-						mt={{ mobS: 'ml' }}
-					>
-						<ButtonComp
-							bg="primary"
-							px="wxs"
-							height="48px"
-							width={{ mobS: '85%', tabS: 'unset' }}
-							mb={{ mobS: 'mm', tabS: 'ws' }}
-							className="hero-cta-l"
+				<a.div
+					style={{
+						...styles,
+					}}
+				>
+					<Box column center>
+						<a.div
+							style={{
+								...styles,
+							}}
 						>
-							<Box as="a" href={WAITLIST_FORM_URL} target="_blank">
-								<Text as="h4">Enter Waitlist</Text>
+							<ResponsiveText
+								mob="h2"
+								tab="h1"
+								desk="h1"
+								text="Simplified platform for creators to launch NFT collections quickly and economically."
+								color="simply-blue"
+								textAlign="center"
+								maxWidth={{ mobS: '27rem', tabS: '50rem', deskM: '59rem' }}
+								id="hero-headline"
+								lineHeight="120%"
+								mb="2%"
+							/>
+							<Text as="b2" maxWidth={{ mobS: '25rem', tabS: '53rem' }} textAlign="center" id="hero-subtext">
+								A simple, easy to use, no-code platform to create NFT smart contracts and launch your NFT projects
+								without any hassle.
+							</Text>
+						</a.div>
+
+						<Box
+							column
+							flexDirection={{ mobS: 'column-reverse', tabS: 'column' }}
+							alignItems="center"
+							mt={{ mobS: 'ml' }}
+						>
+							<a.div
+								style={{
+									...styles,
+								}}
+							>
+								<ButtonComp
+									bg="primary"
+									px="wxs"
+									height="48px"
+									width={{ mobS: '85%', tabS: 'unset' }}
+									mb={{ mobS: 'mm', tabS: 'ws' }}
+									className="hero-cta-l"
+								>
+									<Box as="a" href={WAITLIST_FORM_URL} target="_blank">
+										<Text as="h4">Enter Waitlist</Text>
+									</Box>
+								</ButtonComp>
+							</a.div>
+							<Box center width={{ mobS: '100vw', tabS: '84rem', deskM: '117rem' }} position="relative">
+								<Image src={heroImage} objectFit="cover" className="hero-image" alt="hero-image" />
 							</Box>
-						</ButtonComp>
-						<Box center width={{ mobS: '100vw', tabS: '84rem', deskM: '117rem' }} position="relative">
-							<Image src={heroImage} objectFit="cover" className="hero-image" alt="hero-image" />
 						</Box>
 					</Box>
-				</Box>
+				</a.div>
 				<Box column center mt={{ mobS: 'wm', tabS: 'wxs', deskM: 'wxl' }}>
 					<ResponsiveText
 						tab="h2"
